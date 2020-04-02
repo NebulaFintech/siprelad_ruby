c89cc1cb5588a429e00cc5f74bf7d31a626f55e6
Mauricio Murga <murga.mauricio@gmail.com>
2020-04-02 10:36:59 -0600

    add loan liquidation

--- lib/siprelad/resource.rb
+++ lib/siprelad/resource.rb
@@ -108,6 +108,8 @@ module Siprelad
 
     def self.parse_payment_method(payment_method)
       case payment_method
+      when :loan_liquidation
+        '41'
       when :transfer_payment
         '03'
       when :referenced_payment
